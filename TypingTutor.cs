f681d7b5010d83ad62d64bea86c1db2a7ec54cba
Militchick <39128819+Militchick@users.noreply.github.com>
2018-06-24 19:06:30 +0200

    V.00.2

--- TypingTutor.cs
+++ TypingTutor.cs
@@ -7,7 +7,7 @@ using System.IO;
 class TypingTutor
 {
 
-    public void Main()
+    public static void Main()
     {
         typingtutor();
     }
@@ -16,7 +16,9 @@ class TypingTutor
     {
         string namefile = Console.ReadLine();
 
-        string path = @"c:\typo\" + namefile;
+        Console.WriteLine("Write Directory Path from Local Disc C/ : ");
+        string path = Console.ReadLine();
+        String directory = @"c:/" + path + namefile;
 
         if (!File.Exists(namefile))
         {
@@ -33,7 +35,7 @@ class TypingTutor
         {
             try
             {
-                StreamReader read = new StreamReader(path);
+                StreamReader read = new StreamReader(directory);
                 
                 Console.Write(read.ReadLine());
                 Console.WriteLine();
@@ -123,11 +125,173 @@ class exercise
 
     public string AmountofLines()
     {
-        string name = "Exercise0101.dat";
+        string name = input;
         int lines = 5;
 
         Convert.ToString(lines);
 
         return name + lines;
+    }
+}
+
+class ScreenKeyBoard
+{
+    static void Display()
+    {
+        string[] alphab = new string { "Q", "W", "E", "R", "T", "Y", "U", "I",
+            "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L",  "Z",
+            "X", "C", "V", "B", "N", "M"};
+
+        int r = 0;
+
+        Console.SetCursorPosition(0, -50);
+
+        for (int l = 9; l > 0; l--)
+        {
+            for (int i = 9; i > 0; i--)
+            {
+                for (int j = 9; j > 0; j--)
+                {
+                    for (int k = 9; k > 0; k--)
+                    {
+                        Console.Write("___");
+                        if (k == 0)
+                        {
+                            Console.WriteLine();
+                        }
+                    }
+
+                    Console.Write("| ");
+                    Mark();
+                    Console.Write(alphab[r]);
+                    Unmark();
+                    Console.Write(" |");
+                    r++;
+
+                    if (j == 0)
+                    {
+                        Console.WriteLine();
+                    }
+                }
+                Console.Write("___");
+                if (i == 0)
+                {
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+
+    static void Mark()
+    {
+        if(ConsoleKey.Q = true)
+        {
+            Console.ForegroundColor = green;
+        }
+        else if(ConsoleKey.W = true)
+        {
+            Console.ForegroundColor = green;
+        }
+        else if(ConsoleKey.E = true)
+        {
+            Console.ForegroundColor = green;
+        }
+        else if(ConsoleKey.R = true)
+        {
+            Console.ForegroundColor = green;
+        }
+        else if(ConsoleKey.T = true)
+        {
+            Console.ForegroundColor = green;
+        }
+        else if(ConsoleKey.Y = true)
+        {
+            Console.ForegroundColor = green;
+        }
+        else if(ConsoleKey.U = true)
+        {
+            Console.ForegroundColor = green;
+        }
+        else if(ConsoleKey.I = true)
+        {
+            Console.ForegroundColor = green;
+        }
+        else if(ConsoleKey.O = true)
+        {
+            Console.ForegroundColor = green;
+        }
+        else if (ConsoleKey.P = true)
+        {
+            Console.ForegroundColor = green;
+        }
+        else if (ConsoleKey.A = true)
+        {
+            Console.ForegroundColor = green;
+        }
+        else if (ConsoleKey.S = true)
+        {
+            Console.ForegroundColor = green;
+        }
+        else if (ConsoleKey.D = true)
+        {
+            Console.ForegroundColor = green;
+        }
+        else if (ConsoleKey.F = true)
+        {
+            Console.ForegroundColor = green;
+        }
+        else if (ConsoleKey.G = true)
+        {
+            Console.ForegroundColor = green;
+        }
+        else if (ConsoleKey.H = true)
+        {
+            Console.ForegroundColor = green;
+        }
+        else if (ConsoleKey.J = true)
+        {
+            Console.ForegroundColor = green;
+        }
+        else if (ConsoleKey.K = true)
+        {
+            Console.ForegroundColor = green;
+        }
+        else if (ConsoleKey.L = true)
+        {
+            Console.ForegroundColor = green;
+        }
+        else if (ConsoleKey.Z = true)
+        {
+            Console.ForegroundColor = green;
+        }
+        else if (ConsoleKey.X = true)
+        {
+            Console.ForegroundColor = green;
+        }
+        else if (ConsoleKey.C = true)
+        {
+            Console.ForegroundColor = green;
+        }
+        else if (ConsoleKey.V = true)
+        {
+            Console.ForegroundColor = green;
+        }
+        else if (ConsoleKey.B = true)
+        {
+            Console.ForegroundColor = green;
+        }
+        else if (ConsoleKey.N = true)
+        {
+            Console.ForegroundColor = green;
+        }
+        else if (ConsoleKey.M = true)
+        {
+            Console.ForegroundColor = green;
+        }
+    }
+
+    void Unmark()
+    {
+        Console.ResetColor;
     }
 }
